46a93ae5d6e485311d3fbc09b64bb8627eb6f04c
Muhammad Ahmed Rayyan <ahmedrayyanfamily@gmail.com>
2025-10-22 17:31:34 +0500

    Update email address in ContactUs component

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -9,7 +9,7 @@ const ContactUs: React.FC = () => {
       <div className="social-icons">
         <a href="https://github.com/Muhammad-Ahmed-Rayyan/FixTrack" className="social-icon"><i className="fab fa-github"></i></a>
         <a href="https://www.linkedin.com/posts/sameer-ahmed-ai_fixtrack-civictech-ai-activity-7384722035773632512-ilEE" className="social-icon"><i className="fab fa-linkedin"></i></a>
-        <a href="mailto:sameerahmedrahi@gmail.com" className="social-icon"><i className="fas fa-envelope"></i></a>
+        <a href="mailto:ahmedrayyanfamily@gmail.com" className="social-icon"><i className="fas fa-envelope"></i></a>
       </div>
     </div>
   );
